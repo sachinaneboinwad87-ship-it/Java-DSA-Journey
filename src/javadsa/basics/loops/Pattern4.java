aa53716623c48f047154c71928861757f622d18a
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2026-07-11 23:41:05 +0530

    Added loop pattern programs

---
+++ src/javadsa/basics/loops/Pattern4.java
@@ -0,0 +1,17 @@
+package javadsa.basics.loops;
+//        1
+//        12
+//        123
+//        1234
+//        12345
+public class Pattern4 {
+    static void main(String[] args) {
+        for(int i = 1;i<=5;i++){
+            for(int j =1; j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
