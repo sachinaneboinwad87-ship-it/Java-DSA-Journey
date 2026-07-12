f5ab06831f6cc3ec17c0ed15c22772a20d613bf0
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2026-07-12 12:04:21 +0530

    Added loop patern programs

--- src/javadsa/basics/loops/Pattern1.java
+++ src/javadsa/basics/loops/Pattern1.java
@@ -5,8 +5,8 @@ package javadsa.basics.loops;
 //        * * * *
 public class Pattern1 {
     static void main(String[] args) {
-for (int i =0; i<3;i++){
-    for (int j =0; j<4;j++){
+for (int i =1; i<=4;i++){
+    for (int j =1; j<=4;j++){
         System.out.print("* ");
     }
     System.out.println();
