71427c47527e95467e56f0964453f66dc8839f42
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2026-07-13 23:19:39 +0530

    Added loop advnace program

---
+++ src/javadsa/basics/loops/Pattern9.java
@@ -0,0 +1,27 @@
+package javadsa.basics.loops;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= (2 * n - 1); i++) {
+            System.out.print("* ");
+        }
+    }
+}
+
