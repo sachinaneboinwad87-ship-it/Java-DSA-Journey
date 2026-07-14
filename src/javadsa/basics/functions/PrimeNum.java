ac095ac118d3d1ecdd67eeb09f442ed136fad512
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2026-07-14 15:13:29 +0530

    Given number is prime or not

---
+++ src/javadsa/basics/functions/PrimeNum.java
@@ -0,0 +1,32 @@
+package javadsa.basics.functions;
+
+
+
+public class PrimeNum {
+
+    static void prime() {
+        int num = 17;
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println("Given number is Prime");
+        } else {
+            System.out.println("Given number is Not Prime");
+        }
+    }
+
+    public static void main(String[] args) {
+        prime();
+    }
+}
