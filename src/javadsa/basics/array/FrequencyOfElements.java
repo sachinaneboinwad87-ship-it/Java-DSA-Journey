45c4f1a80ff9d17f15acd9a91697c69a0d18748f
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2026-07-17 22:36:02 +0530

    Added Frequency count program

---
+++ src/javadsa/basics/array/FrequencyOfElements.java
@@ -0,0 +1,43 @@
+package javadsa.basics.array;
+import java.util.Scanner;
+
+public class FrequencyOfElements {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Frequency of each element:");
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i]) {
+                continue;
+            }
+
+
+        int count = 1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                visited[j] = true;
+            }
+        }
+
+        System.out.println(arr[i] + " = " + count);
+    }
+
+        sc.close();
+}
+}
